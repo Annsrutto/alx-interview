dca1fddff4a685b83af7172c51c6f7f15426b85a
Annsrutto <annsrutto567@gmail.com>
2024-07-04 23:59:31 +0300

    N-Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""a program that solves the N queens problem
+using Python and the backtracking approach."""
+
 import sys
+
 
 def is_valid(board, row, col):
     for i in range(row):
@@ -8,6 +12,7 @@ def is_valid(board, row, col):
            board[i] + i == col + row:
             return False
     return True
+
 
 def solve_nqueens(N, row, board, solutions):
     if row == N:
@@ -19,6 +24,7 @@ def solve_nqueens(N, row, board, solutions):
             solve_nqueens(N, row + 1, board, solutions)
             board[row] = -1
 
+
 def print_solutions(solutions):
     for sol in solutions:
         solution = []
@@ -26,17 +32,18 @@ def print_solutions(solutions):
             solution.append([i, sol[i]])
         print(solution)
 
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
@@ -45,6 +52,7 @@ def main():
     board = [-1] * N
     solve_nqueens(N, 0, board, solutions)
     print_solutions(solutions)
+
 
 if __name__ == "__main__":
     main()
